0d3346d1caf0f578f378c35dc8571105919e00eb
Ethan Teng <ethanteng1212@qq.com>
2024-09-14 17:09:10 +0800

    chore: let's try it again

--- Formula/ezcfg.rb
+++ Formula/ezcfg.rb
@@ -4,9 +4,9 @@
 class Ezcfg < Formula
     desc "Use soft link to centrally manage all your personal profiles"
     homepage "https://github.com/tim101010101/ezcfg"
-    version "0.0.1"
-    url "https://github.com/tim101010101/ezcfg/releases/download/v0.0.1/ezcfg_0.0.1.tar.gz"
-    sha256 "2a180ba7ad87c452cae291fd7e8c5b7ed6127c25515cc7c46194bebdccc1974b"
+    version "0.0.2"
+    url "https://github.com/tim101010101/ezcfg/releases/download/v0.0.2/ezcfg_0.0.2.tar.gz"
+    sha256 "5491bc502168d201cac2ba8cc866c7d0ebe8285f17e7e10ff1629e42c95d4d9f"
     license ""
   
     def install
