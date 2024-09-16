4b1dc223af0f98e0d8197e1d4184fc57dca2236d
Ethan Teng <ethanteng1212@qq.com>
2024-09-16 22:53:44 +0800

    release: 0.0.3

--- Formula/ezcfg.rb
+++ Formula/ezcfg.rb
@@ -2,12 +2,13 @@
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Ezcfg < Formula
+    version "0.0.3"
+
     desc "Use soft link to centrally manage all your personal profiles"
     homepage "https://github.com/tim101010101/ezcfg"
-    version "0.0.2"
-    url "https://github.com/tim101010101/ezcfg/releases/download/v0.0.2/ezcfg_0.0.2.tar.gz"
-    sha256 "5491bc502168d201cac2ba8cc866c7d0ebe8285f17e7e10ff1629e42c95d4d9f"
-    license ""
+    
+    url "https://github.com/tim101010101/ezcfg/releases/download/#{version}/ezcfg_#{version}.tar.gz"
+    sha256 "590eeaf93dd4958c62d4a1c07efeb0a0285b1b529cfed6190a9b960dd7d5873a"
   
     def install
       bin.install "ezcfg"
