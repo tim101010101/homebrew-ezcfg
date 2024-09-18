608e7163cca7295cf087579eebe6a4bdcb34532f
Ethan Teng <ethanteng1212@qq.com>
2024-09-18 17:25:12 +0800

    release: version 0.0.4

--- Formula/ezcfg.rb
+++ Formula/ezcfg.rb
@@ -1,14 +1,11 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
 class Ezcfg < Formula
-    version "0.0.3"
+    version "0.0.4"
 
     desc "Use soft link to centrally manage all your personal profiles"
     homepage "https://github.com/tim101010101/ezcfg"
     
     url "https://github.com/tim101010101/ezcfg/releases/download/#{version}/ezcfg_#{version}.tar.gz"
-    sha256 "590eeaf93dd4958c62d4a1c07efeb0a0285b1b529cfed6190a9b960dd7d5873a"
+    sha256 "a9fd6fb8be5c9974bc690e369fd64a18c117f1e8e1bd839f78992a2d962d5b7d"
   
     def install
       bin.install "ezcfg"
